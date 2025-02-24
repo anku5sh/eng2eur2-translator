5157d544252ca4e2356a7ceaf89ad4433b65f6a9
anku5sh <ankush4play@yahoo.co.in>
2025-02-24 20:10:38 +0100

    Update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 source .venv/bin/activate
-pyinstaller --noconfirm --onefile --windowed \
+pyinstaller --noconfirm --windowed \
   --name "eng2eur2" \
   --add-data "src:src" \
   --hidden-import PyQt6 \
   --hidden-import deep_translator \
+  --collect-submodules "tkinter" \
+  --runtime-hook <(echo "import os; os.environ['TCL_LIBRARY'] = 'Tcl'; os.environ['TK_LIBRARY'] = 'Tk'") \
   src/main.py
