1f9e4ef019087ebae2a71ff5a67960c5fffa30cc
anku5sh <ankush4play@yahoo.co.in>
2025-02-24 21:09:47 +0100

    Update build_mac.sh

--- build_mac.sh
+++ build_mac.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 source .venv/bin/activate
+
+TCL_TK_PREFIX=$(brew --prefix tcl-tk)
+
 pyinstaller --noconfirm --windowed \
   --name "eng2eur2" \
   --add-data "src:src" \
   --hidden-import PyQt6 \
   --hidden-import deep_translator \
   --collect-submodules "tkinter" \
-  --runtime-hook <(echo "import os; os.environ['TCL_LIBRARY'] = 'Tcl'; os.environ['TK_LIBRARY'] = 'Tk'") \
+  --paths "$TCL_TK_PREFIX/lib" \
+  --runtime-hook <(echo "import os; os.environ['TCL_LIBRARY'] = '$TCL_TK_PREFIX/lib/Tcl.framework/Versions/8.6/Resources/Scripts/tcl8.6'; os.environ['TK_LIBRARY'] = '$TCL_TK_PREFIX/lib/Tk.framework/Versions/8.6/Resources/Scripts/tk8.6'") \
   src/main.py
